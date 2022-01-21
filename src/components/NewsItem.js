813f3b33f30e1e0ef675e6799452af718840dfd8
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2022-01-21 23:00:37 +0530

    Added Navbar and News Component

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class NewsItem extends Component {
+  render() {
+    return (
+        <div>
+            This is a news item it is inside News.js
+        </div>
+    );
+  }
+}
+
+export default NewsItem;
