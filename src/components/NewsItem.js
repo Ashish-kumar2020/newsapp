c33f244f13b0ba64bfe78b7e5f5a93d506240c66
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2022-01-27 21:39:37 +0530

    Added author name and time for news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 export class NewsItem extends Component {
   
   render() {
-    let { title, description,imageUrl,newsUrl } = this.props;
+    let { title, description,imageUrl,newsUrl,author, date } = this.props;
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
@@ -12,6 +12,7 @@ export class NewsItem extends Component {
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
           <p className="card-text">{description}...</p>
+          <p className="card-text"><small className='text-muted'><span className='badge rounded-pill bg-success'>By {author?author:"Unknown"}</span> on {new Date(date).toGMTString()}</small></p>
           <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-primary btn-sm btn-dark">Read More</a>
         </div>
       </div>
