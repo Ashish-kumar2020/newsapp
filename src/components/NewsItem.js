f41d3e92b1041bc6e6e7c76864d42f0d31f80bd4
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2022-01-28 22:25:25 +0530

    Convert Code from class component to functional component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,24 +1,24 @@
 
-import React, { Component } from 'react';
 
-export class NewsItem extends Component {
 
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date } = this.props;
-    return (
-      <div className='my-3'>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text"><small className='text-muted'><span className='badge rounded-pill bg-success'>By {author ? author : "Unknown"}</span> on {new Date(date).toGMTString()}</small></p>
-            <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-primary btn-sm btn-dark">Read More</a>
-          </div>
+const NewsItem = (props) => {
+
+
+  let { title, description, imageUrl, newsUrl, author, date } = props;
+  return (
+    <div className='my-3'>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text"><small className='text-muted'><span className='badge rounded-pill bg-success'>By {author ? author : "Unknown"}</span> on {new Date(date).toGMTString()}</small></p>
+          <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-primary btn-sm btn-dark">Read More</a>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+
 }
 
 export default NewsItem;
